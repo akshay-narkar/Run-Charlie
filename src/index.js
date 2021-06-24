f0ffaa0235d6392a4d9bd7dcffb598cc3872942c
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-24 19:37:01 +0530

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import 'phaser';
+import Phaser from 'phaser';
 
-import { SimpleScene } from './scenes/simple-scene.js';
+import './stylesheet.css';
+import gameConfig from './Config/config';
+import playGame from './Scenes/GameScene';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
+import OptionsScene from './Scenes/OptionsScene';
+import CreditsScene from './Scenes/CreditsScene';
+import OverGame from './Scenes/Gamover';
+import FormScene from './Scenes/formscene';
+import highscore from './Scenes/highscorescene';
 
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
-};
+import Model from './Model';
 
-new Phaser.Game(gameConfig);
+class Game extends Phaser.Game {
+  constructor() {
+    super(gameConfig);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('PlayGame', playGame);
+    this.scene.add('Gameover', OverGame);
+    this.scene.add('Form', FormScene);
+    this.scene.add('highscore', highscore);
+
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
+
+// const gameConfig = {
+//   width: 680,
+//   height: 400,
+//   scene: SimpleScene
+// };
+
+// new Phaser.Game(gameConfig);

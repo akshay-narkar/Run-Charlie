c3f9c23ae233244805a4aec8b7d18052fbaf4aa6
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-25 22:46:39 +0530

    Update helper methods

--- src/helper/fetching.js
+++ src/helper/fetching.js
@@ -1,36 +1,3 @@
-// const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/i6OiNUavZ2OszfY3wkNT/scores/'
-
-// // export default function displaydata() {
-// //     fetch(url, { mode: "cors", })
-// //       .then(function (response) {
-// //         // console.log(response.json());
-// //         return response.json();
-
-// //       })
-// //       .catch((e) => {
-// //         alert( e + " nothing found");
-// //       })
-
-// //     }
-
-// export default async function displaydata() {
-
-//   const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/i6OiNUavZ2OszfY3wkNT/scores/`;
-
-//   const response = await fetch(url);
-//   // if HTTP-status is 200-299
-//   // get the response body (the method explained below)
-//   if (response.ok) {
-//    json1 = await response.json();
-//    console.log(json1)
-//    return json1;
-
-//   }
-//   else {
-//     return error.json()
-//   }
-// }
-
 const getData = async (url) => {
   try {
     const response = await fetch(url);
@@ -42,7 +9,7 @@ const getData = async (url) => {
 };
 
 const sortPlayers = (arr) => {
-  arr.sort((a, b) => a.score - b.score);
+  arr.sort((a, b) => b.score - a.score);
 };
 
 const getPlayers = async (url) => {
@@ -51,4 +18,21 @@ const getPlayers = async (url) => {
   return arr;
 };
 
-export { getData, getPlayers };
+const updatescore = async (url) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: localStorage.getItem('player'), score: localStorage.getItem('highscore') }),
+      mode: 'cors',
+    });
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { getData, getPlayers, updatescore };

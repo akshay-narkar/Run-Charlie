86065f3c73e292229505a00ff53cf8dbca82b1d8
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-25 15:13:56 +0530

    Load bg image

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -7,6 +7,8 @@ export default class BootScene extends Phaser.Scene {
 
   preload() {
     this.load.image('logo', 'assets/mario.png');
+        this.load.image('mount', 'assets/mount2.jpg');
+
   }
 
   create() {

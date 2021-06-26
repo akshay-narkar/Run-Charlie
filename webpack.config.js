84eea8777c8b1f93b0116699811766b78703f4a5
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-26 20:00:43 +0530

    Update main output file

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
       },
 
       {
-         test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
